abbfb31e4335d9766c2db895c14d61c68cbb4a0c
Robert Boczek <robertbo@fb.com>
2019-11-18 22:19:34 -0500

    Initial Commit

---
+++ WeatherApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Robert Boczek on 11/18/19.
+//  Copyright © 2019 Robert Boczek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
